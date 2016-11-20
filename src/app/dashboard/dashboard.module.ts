3b9935a52c55ea1d3a07d38b7b060f62b2e175b2
Vladimir Semashko <vladimir.semashko@intexsoft.by>
2016-11-20 18:37:51 +0300

    Add dynamically generated components with dynamic template

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { TranscludeComponent } from './transclude.component';
 import { DynamicViewGeneratorComponent } from './dynamic-view-generator.component';
-import { DynamicModule } from '../dynamic/dynamic.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
